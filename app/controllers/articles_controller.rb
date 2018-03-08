a81b5fedce2b3618235027ef539efdde2d78e15e
Amourlive <aleksei_mackevich@outlook.com>
2018-03-09 01:26:08 +0300

    Implement articles#edit articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,9 @@
 class ArticlesController < ApplicationController # :nodoc:
-
   def index
     @articles = Article.all
   end
 
-  def show;
+  def show
     @article = Article.find(params[:id])
   end
 
@@ -12,11 +11,24 @@ class ArticlesController < ApplicationController # :nodoc:
 
   def create
     @article = Article.new(article_params)
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
     end
   end
 
