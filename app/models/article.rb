5930a245f4c7b617fedc224c70e83156ddcea253
Amourlive <aleksei_mackevich@outlook.com>
2018-03-12 19:37:52 +0300

    Add length validate for text and title

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord #:nodoc:
   validates :title, presence: true
   validates :text, presence: true
+  validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed" }
+  validates :text, length: { maximum: 4000, too_long: "%{count} characters is the maximum allowed" }
   has_many :comments
 
   def subject
