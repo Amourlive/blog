093d7e362e67116ba98f0f1b2405d3695a6b3e92
Amourlive <aleksei_mackevich@outlook.com>
2018-03-12 19:38:13 +0300

    Add length validate for body

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 # Comment for article
 class Comment < ApplicationRecord
   belongs_to :article
+  validates :body, length: { maximum: 4000, too_long: "%{count} characters is the maximum allowed" }
 end
