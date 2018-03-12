fed8e052576f3bd96d5e745f357cd4ebe0e294fc
Amourlive <aleksei_mackevich@outlook.com>
2018-03-12 20:20:37 +0300

    Add validation for title and text length for article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,6 +10,15 @@ describe Article do
     it { should have_many :comments }
   end
 
+  describe 'max length title' do
+    it { should validate_length_of(:title).is_at_most(140).with_long_message('140 characters is the maximum allowed') }
+  end
+
+  describe 'max length text' do
+    it { should validate_length_of(:text).is_at_most(4000).with_long_message('4000 characters is the maximum allowed') }
+  end
+
+
   describe '#subject' do
     it 'return the article title' do
       # создаем объект article хитрым способом
