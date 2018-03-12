800293265916833c8b43f74612bada0e11f0700a
Amourlive <aleksei_mackevich@outlook.com>
2018-03-12 13:59:38 +0300

    add rspec-rails without changes

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,5 @@
-require 'spec_helper'
+require 'rails_helper'
 
-RSpec.describe Contact, :type => :model do
-  it { should validates_presence_of(:email) }
+describe Contact do
+  it { should validate_presence_of :email }
 end
