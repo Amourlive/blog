da719b6b1ccf258971c32568cc6b3323b11adaee
Amourlive <aleksei_mackevich@outlook.com>
2018-03-12 20:23:53 +0300

    Add validation for body length for comments model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 describe Comment do
-  it { should belong_to :article }
+  describe 'comments belong to article' do
+    it { should belong_to :article }
+  end
+  describe 'max length body' do
+    it { should validate_length_of(:body).is_at_most(4000).with_long_message('4000 characters is the maximum allowed') }
+  end
 end
